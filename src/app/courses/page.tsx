0a6d8d5b79640490841a25620bada1813e0521fa
Arsalan <105946415+arsalankaiser@users.noreply.github.com>
2025-08-25 18:25:01 +0530

    Update page.tsx
    comment on link

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -42,7 +42,7 @@ export default function page() {
                 <CardItem
                   translateZ={20}
                   as={Link}
-                  href="https://www.google.com"
+{/*                   href="https://www.google.com" */}
                   target="__blank"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
